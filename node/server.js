e42a18a1b6d52ccea5da6b70e29c922312b5d2c8
peff <p.effantin@gmail.com>
2018-04-25 18:48:33 +0200

    fonction addStartup operationelle

--- node/server.js
+++ node/server.js
@@ -5,44 +5,28 @@ firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
     databaseURL: "https://sourcing-stratups.firebaseio.com"
 });
-var database = firebase.database();
+//var database = firebase.database();
 
 //fetch the object startup and prepare instanciation
 var Startup = require("./startup.js");
-/*
-const test = new Startup("Paul", 
-                        "France", 
-                        "2009", 
-                        "blablabla", 
-                        "Company", 
-                        5, 
-                        9, 
-                        ["AI"], 
-                        "p.ef@d.com", 
-                        false, 
-                        "www.google.com");
 
-console.log(test)
-*/
+//add a startup en creant un id manuellement
 
-//add a startup
-
-var id = 32003
+var id = 32005
 var object = new Startup("Fabio Kuhn", "United Kingdom", 2016, "Provider of an analytics platform designed to focus on the oil markets. The company uses artificial intelligence to analyse and interpret data on a massive scale and absorbs billions of data points from hundreds of sources to show past present and future flows of oil worldwide enabling traders to outperform the competition with actionable real-time market intelligence.",
     "Vortexa", "", 1985, ["Business/Productivity Software", "Media and Information Services (B2B)", "Other EnergyArtificial Intelligence & Machine Learning"],
     "fabio.kuhn@vortexa.com", "?", "vortexa.com");
 
-var startupDetail = firebase.database().ref("/" + id );
+function addStartup(id, object){
+    var startupDetail = firebase.database().ref("/" + id);
+    startupDetail.set({ object });
+}
 
-startupDetail.set({object});
+addStartup(id, object);
 
-//const essai = new startup.constructor('jean', 'USA', '2009', 'blablabla', 'company', 2, 32090, 'AI', 'mail@mail.com', true, 'www.google.com');
-//console.log(essai)
-/*
-function addStartup(contact, country, creation_date, descriptif, entreprise, funds, id, keywords, mail, target, url) {
-    var newStartup = essai;
 
-}*/
+
+
 
 /*
 //get a startup descriptif by id
