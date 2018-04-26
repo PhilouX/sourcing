b4f346f0945c7e610c7c624eb0bcca229c46cad5
peff <p.effantin@gmail.com>
2018-04-26 11:12:40 +0200

    export des modules add et get

--- node/database.js
+++ node/database.js
@@ -1,3 +1,4 @@
+
 //initialize firebase and get a firebase reference = database
 var firebase = require("firebase-admin");
 var serviceAccount = require("firebase-admin/serviceAccountKey.json");
@@ -10,19 +11,25 @@ var database = firebase.database();
 //fetch the object startup and prepare instanciation
 var Startup = require("./startup");
 
-//add a startup document
-function addStartup(id, object) {
-    var startupNew = firebase.database().ref("/" + id);
-    startupNew.set({ object });
+module.exports = {
+
+        //add a startup document
+        addStartup: function addStartup(id, object) {
+            var startupNew = firebase.database().ref("/" + id);
+            startupNew.set({ object });
+        },
+
+        //get a startup descriptif by id
+        getStartup: function getStartup(id){
+            var startupDetail = firebase.database().ref('/' + id);
+            startupDetail.on('value', function (snapshot) {
+            console.log(snapshot.val());
+            });
+        }
 }
 
-//get a startup descriptif by id
-function getStartup(id){
-    var startupDetail = firebase.database().ref('/' + id);
-    startupDetail.on('value', function (snapshot) {
-    console.log(snapshot.val());
-    });
-}
+
+
 
 //getStartup(20);
 //addStartup(id, object);
@@ -33,38 +40,14 @@ var id = 32006
 var object = new Startup("Superman", "United Kingdom", 2016, "Provider of an analytics platform designed to focus on the oil markets. The company uses artificial intelligence to analyse and interpret data on a massive scale and absorbs billions of data points from hundreds of sources to show past present and future flows of oil worldwide enabling traders to outperform the competition with actionable real-time market intelligence.",
     "International one", "", 1985, ["Business/Productivity Software", "Media and Information Services (B2B)", "Other EnergyArtificial Intelligence & Machine Learning"],
     "p@v.com", "?", "google.com");
-*/
-
-
-
-
-
-
-/*
 
 //update startup details by id
 var updateDetails = firebase.database().ref()
-        .child('/'+ startupId + '/').set({contact : "blabl",
-            country : "United Kingdom",
-            creation_date : 2016,
-            descriptif : "Provider of an analytics platform designed to focus on the oil markets. The company uses artificial intelligence to analyse and interpret data on a massive scale and absorbs billions of data points from hundreds of sources to show past present and future flows of oil worldwide enabling traders to outperform the competition with actionable real-time market intelligence.",
-            entreprise : "company",
-            funds : "",
-            id : 1985,
-            keywords : ["Business/Productivity Software", "Media and Information Services (B2B)", "Other EnergyArtificial Intelligence & Machine Learning"],
-            mail : "mail",
-            target : "?",
-            url : "url"});
+        .child('/'+ startupId + '/').set({object});
 
 //update contactname by id
 var updateContact = firebase.database().ref()
     .child('/' + startupId + '/').update({
         "/contact": "bioman",
     });
-*/
-//add a new startup
-
-
-
-
-
+    */
