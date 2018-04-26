f5c7ebd4f98ded0920709f182057af174799f603
peff <p.effantin@gmail.com>
2018-04-26 22:32:47 +0200

    rajout de la fonction add + vue formulaire

--- node/server.js
+++ node/server.js
@@ -4,12 +4,17 @@ var app = express();
 var database = require("./database");
 
 //Test sans vue
-//var test = database.getStartup(10);
+//var test = database.getStartup(10, "contact");
 //console.log(test);
+
 
 app.get('/startup/:id', function (req, res) {
     var startup = database.getStartup(req.params.id);
-    res.render('home.ejs', {id: req.params.id, startup: startup});
+    res.render('home.ejs', { id: req.params.id, startup: startup });
+    //console.log(startup);
+}).get('/startup/add/', function(req, res) {
+    var startup = database.addStartup(id, object);
+    res.render('home.ejs');
 })
    .use(function (req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
