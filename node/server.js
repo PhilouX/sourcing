f49d88bdd5e74e2fa5f14db06aa2fd7afbae3c6a
peff <p.effantin@gmail.com>
2018-05-28 19:48:12 +0200

    add list form query

--- node/server.js
+++ node/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var database = require("./database");
 var logger = require('morgan')
 var firebase = require("firebase-admin");
 //Instanciantion de l'acces à la base
@@ -10,9 +9,10 @@ var firebase = firebase.initializeApp({
     databaseURL: "https://sourcing-stratups.firebaseio.com"
 });
 
-
+//create an instance of express
 var app = express();
 
+//we use ejs
 app.set('view engine', 'ejs');
 
 //to storage file if needed.
@@ -26,41 +26,54 @@ app.use(bodyParser.urlencoded({extended: false}));
 // for the logs
 app.use(logger('dev'));    
 
-app.get('/startup/:id', function (req, res) {
-    var startupDetail = firebase.database().ref("/" + req.params.id);
-    startupDetail.on('value', function (snapshot) {
+//ne fonctionne pas, besoin de connaitre le path avec le bon id
+app.get('/startup/:entreprise', function (req, res) {
+    var startupDetail = firebase.database().ref("/entreprises" + req.params.entreprise);
+    startupDetail.once('value', function (snapshot) {
         var startupProfile = snapshot.val();
         //console.log(startupProfile.contact);
         res.render('home.ejs', {
-            id: req.params.id, startupProfile: startupProfile}); 
+            entreprise: req.params.entreprise, startupProfile: startupProfile}); 
     })
-})/*
-.post('/startup/add/', function(req, res) {
-    //var entreprise = req.body.entreprise;
-    //res.render('startup.ejs', {data: entreprise});
-    var entreprise = req.body.entreprise;
-    var startup = database.addStartup(001, entreprise);
-    //res.render('home.ejs', {data : });
-    
 })
-    addStartup: function addStartup(id, object) {
-        var startupNew = firebase.database().ref("/" + id);
-        startupNew.set({ object });
-    },
+//access to the form to add an entreprise
+app.get('/add/', function (req, res){
+    res.render('startupForm.ejs')
+})
+//adding the entreprise to db and return a page
+app.post('/add/', function (req, resp) {
+    var entreprise = req.body.entreprise;
+    var url = req.body.url;
+    var keywords = req.body.keywords;
+    var descriptif = req.body.descriptif;
 
-    //update startup details by id
-var updateDetails = firebase.database().ref()
-        .child('/'+ startupId + '/').set({object});
+    var startupRef = firebase.database().ref("/entreprises");
+    startupRef.push({ entreprise: entreprise, url: url, keywords: keywords, descriptif: descriptif });
 
-//update contactname by id
-var updateContact = firebase.database().ref()
-    .child('/' + startupId + '/').update({
-        "/contact": "bioman",
-    });
-*/
-   .use(function (req, res, next) {
+    resp.render('results.ejs', { entreprise: entreprise, url: url, keywords: keywords, descriptif: descriptif });
+});
+
+//query form linked with lunr client side
+app.get('/query/', function (req, res) {
+    res.render('queryResult.ejs')
+})
+
+//display the list of entreprise in the db
+app.get('/list/', function (req, res){
+        var startupsRef = firebase.database().ref("/entreprises");
+        startupsRef.once('value', (function (snapshot) {
+            var data = snapshot.val();
+            if (!data) {
+                data = {}
+            }
+            console.log(data);
+            res.render('startuplist.ejs', { startups: snapshot.val() })
+    }))})
+
+//l'erreur 404
+app.use(function (req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
-    res.status(404).send('startup introuvable !');
+    res.status(404).send('page introuvable !');
 });
 
 app.listen(8080);
